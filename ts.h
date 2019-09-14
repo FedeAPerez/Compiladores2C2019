4bfcf48a4eed819272ef7c45f8b6e430774187f1
Federico Pérez <federico.fperez@mercadolibre.com>
2019-09-14 20:49:14 -0300

    fixed

--- ts.h
+++ ts.h
@@ -1,8 +1,8 @@
 #include "stdio.h"
 
 #define FILE_NAME "ts.txt"
-#define READ "r"
-#define APPEND "a"
+#define READ_FILE "r"
+#define APPEND_FILE "a"
 
 void initTs(FILE *);
 void saveTs(char *, char *, char *, char *);
@@ -14,17 +14,17 @@ void initTs(FILE *fp)
 
 void saveTs(char *text, char *type, char *value, char *length)
 {
-    FILE *fp = fopen(FILE_NAME, READ);
+    FILE *fp = fopen(FILE_NAME, READ_FILE);
     if (!fp)
     {
-        fp = fopen(FILE_NAME, APPEND);
+        fp = fopen(FILE_NAME, APPEND_FILE);
         initTs(fp);
         fprintf(fp, "\n%-35s %-20s %-45s %-20s", text, type, value, length);
         fclose(fp);
     }
     else
     {
-        fp = fopen(FILE_NAME, APPEND);
+        fp = fopen(FILE_NAME, APPEND_FILE);
         fprintf(fp, "\n%-35s %-20s %-45s %-20s", text, type, value, length);
         fclose(fp);
     }

6e9f5b9cab9f8fbc25d056fb197e6c12e199d411
Federico Pérez <federico.fperez@mercadolibre.com>
2019-09-15 16:50:56 -0300

    tests on windows

--- build.bat
+++ build.bat
@@ -4,7 +4,23 @@ flex Lexico.l
 pause
 c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o .\Primera.exe
 pause
+echo "PRUEBA - OK"
+type .\tests\prueba.txt | .\Primera.exe
+pause
+echo "PRUEBA PARENTESIS - OK"
+type .\tests\parentesis.txt | .\Primera.exe
+pause
+echo "PRUEBA STRING RECHAZADA - ERROR"
+type .\tests\string.rejected.txt | .\Primera.exe
+pause
+echo "PRUEBA ENTERO RECHAZADO - ERROR"
+type .\tests\int.rejected.txt | .\Primera.exe
+pause
+echo "PRUEBA MOD/DIV - OK"
 type .\tests\mod-div.txt | .\Primera.exe
+pause
+echo "PRUEBA REPEAT - OK"
+type .\tests\repeat.txt | .\Primera.exe
 pause
 del y.tab.c
 del y.tab.h

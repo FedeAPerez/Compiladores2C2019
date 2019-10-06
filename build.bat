f5423fffd3bd78caee9e0cd23873bf209aa0319f
unknown <jenniferyb@hotmail.com>
2019-10-06 02:45:33 -0300

    se agregan los test a build

--- build.bat
+++ build.bat
@@ -8,9 +8,27 @@ pause
 del ts.txt
 del intermedia.txt
 del status.txt
-echo "PRUEBA - OK"
+echo "PRUEBA - OK TERCETOS ASIG MULT"
 type .\tests\prueba-tercetos-if.txt | .\build\Primera.exe
 pause
+echo "PRUEBA - OK"
+type .\tests\prueba.txt | .\build\Primera.exe
+pause
+echo "PRUEBA PARENTESIS - OK"
+type .\tests\parentesis.txt | .\build\Primera.exe
+pause
+echo "PRUEBA STRING RECHAZADA - ERROR"
+type .\tests\string.rejected.txt | .\build\Primera.exe
+pause
+echo "PRUEBA ENTERO RECHAZADO - ERROR"
+type .\tests\int.rejected.txt | .\build\Primera.exe
+pause
+echo "PRUEBA MOD/DIV - OK"
+type .\tests\mod-div.txt | .\build\Primera.exe
+pause
+echo "PRUEBA REPEAT - OK"
+type .\tests\repeat.txt | .\build\Primera.exe
+type .\tests\prueba-tercetos-if.txt | .\build\Primera.exe
 del y.tab.c
 del y.tab.h
 del y.output

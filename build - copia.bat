d5107101f1a5e07ea9d411861e3083c79e702cd5
unknown <jenniferyb@hotmail.com>
2019-10-06 15:32:35 -0300

    pruebas

---
+++ build - copia.bat
@@ -0,0 +1,35 @@
+mkdir build
+C:\GnuWin32\bin\bison -dy Sintactico.y
+pause
+C:\GnuWin32\bin\flex Lexico.l
+pause
+C:\TDM-GCC-64\bin\gcc.exe lex.yy.c y.tab.c -o .\build\Primera.exe
+pause
+del ts.txt
+del intermedia.txt
+del status.txt
+echo "PRUEBA - OK TERCETOS ASIG MULT"
+type .\tests\prueba-tercetos-if.txt | .\build\Primera.exe
+pause
+echo "PRUEBA - OK"
+type .\tests\prueba.txt | .\build\Primera.exe
+pause
+echo "PRUEBA PARENTESIS - OK"
+type .\tests\parentesis.txt | .\build\Primera.exe
+pause
+echo "PRUEBA STRING RECHAZADA - ERROR"
+type .\tests\string.rejected.txt | .\build\Primera.exe
+pause
+echo "PRUEBA ENTERO RECHAZADO - ERROR"
+type .\tests\int.rejected.txt | .\build\Primera.exe
+pause
+echo "PRUEBA MOD/DIV - OK"
+type .\tests\mod-div.txt | .\build\Primera.exe
+pause
+echo "PRUEBA REPEAT - OK"
+type .\tests\repeat.txt | .\build\Primera.exe
+pause
+del y.tab.c
+del y.tab.h
+del y.output
+del Primera.exe
--- build.bat
+++ build.bat
@@ -11,24 +11,6 @@ del status.txt
 echo "PRUEBA - OK TERCETOS ASIG MULT"
 type .\tests\prueba-tercetos-if.txt | .\build\Primera.exe
 pause
-echo "PRUEBA - OK"
-type .\tests\prueba.txt | .\build\Primera.exe
-pause
-echo "PRUEBA PARENTESIS - OK"
-type .\tests\parentesis.txt | .\build\Primera.exe
-pause
-echo "PRUEBA STRING RECHAZADA - ERROR"
-type .\tests\string.rejected.txt | .\build\Primera.exe
-pause
-echo "PRUEBA ENTERO RECHAZADO - ERROR"
-type .\tests\int.rejected.txt | .\build\Primera.exe
-pause
-echo "PRUEBA MOD/DIV - OK"
-type .\tests\mod-div.txt | .\build\Primera.exe
-pause
-echo "PRUEBA REPEAT - OK"
-type .\tests\repeat.txt | .\build\Primera.exe
-pause
 del y.tab.c
 del y.tab.h
 del y.output
